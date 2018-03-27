44a98750a6c36e39ae3c69e3d66ea9416320fd28
Jake <kartchner56jake@gmail.com>
2018-03-27 14:27:52 -0600

    added react strap and inputs

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -1,9 +1,62 @@
 import React, { Component } from 'react';
+import {
+	Col,
+	Row
+} from 'reactstrap';
 
 class MadlibForm extends Component {
-    render() {
-      return <h1>Bottega Mad Libs Form Component</h1>;
-    }
+
+	handleChange = function() {
+		console.log('trying to handle change')
+	}
+
+
+	render() {
+		return (
+			<Row style={{textAlign: 'center', color: 'white'}}>
+				<Col md="3" classname='inputWrapper'>
+					<Row>
+						<Col md="2">
+							<label classname="greenLabel">1</label>
+						</Col>
+						<Col nd='10'>
+							<input placeholder='Color' type='text'  onChange={this.handleChange}/>
+						</Col>
+					</Row>
+				</Col>
+				<Col md="3" classname='inputWrapper'>
+					<Row>
+						<Col md="2">
+							<label classname="greenLabel">1</label>
+						</Col>
+						<Col nd='10'>
+							<input placeholder='Color' type='text'  onChange={this.handleChange}/>
+						</Col>
+					</Row>
+				</Col>
+				<Col md="3" classname='inputWrapper'>
+					<Row>
+						<Col md="2">
+							<label classname="greenLabel">1</label>
+						</Col>
+						<Col nd='10'>
+							<input placeholder='Color' type='text'  onChange={this.handleChange}/>
+						</Col>
+					</Row>
+				</Col>
+				<Col md="3" classname='inputWrapper'>
+					<Row>
+						<Col md="2">
+							<label classname="greenLabel">1</label>
+						</Col>
+						<Col nd='10'>
+							<input placeholder='Color' type='text'  onChange={this.handleChange}/>
+						</Col>
+					</Row>
+				</Col>
+			</Row>
+		);
+	}
 }
 
 export default MadlibForm;
